47a7771aa4a194b1446bf2faaf9c767f2140c2c7
= <developer.benjamin@blackdish.mx>
2020-06-13 17:50:10 -0500

    firt commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './Hero';
+
+export const HEROES: Hero[] = [
+	{id: 1, name: 'All Might'},
+	{id: 2, name: 'Endeavor'},
+	{id: 3, name: 'Hawks'},
+	{id: 4, name: 'Edgeshot'},
+	{id: 5, name: 'Mirko'},
+	{id: 6, name: 'Crust'},
+	{id: 7, name: 'Kamui Woods'},
+	{id: 8, name: 'Wash'},
+	{id: 9, name: 'Fatgum'},
+	{id: 10, name: 'Ryukyu'}
+];
